a5750c255c96f76b5d3c9a0f0950f2db3cecb0ec
Kagwi <kelvinmwas8@gmail.com>
2025-02-11 19:46:33 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
 import AdminDashboard from './pages/AdminDashboard';
 import ProductManagement from './pages/ProductManagement';
 import BlogManagement from './pages/BlogManagement';
